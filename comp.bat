68c8b4e3a3210943c5fb4c203247c8a7b207aee6
PZ <60358202+Alg0ritmus@users.noreply.github.com>
2024-02-08 13:00:02 +0100

    update of bat file

--- comp.bat
+++ comp.bat
@@ -1,3 +1,3 @@
-gcc -Os -Wall -Wno-unused-function -Werror -Wextra -o main main.c gf25519.c utils.c modl.c ristretto255.c && main.exe
+gcc -Os -Wall -Wno-unused-function -Werror -Wextra -o main main.c gf25519.c utils.c modl.c ristretto255.c prng.c xxhash.c && main.exe
 
 rem NOTE: You can set various flags in config.h
